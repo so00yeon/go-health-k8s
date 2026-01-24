3b9ac43a84ac6824c4b177da4138344f8c20a8a5
soso <hazels201@gmail.com>
2026-01-24 17:17:01 +0900

    feat: 구조화 로깅 적용 (slog)

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,21 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"log"
+	"log/slog"
 	"net/http"
+	"os"
 
 	"github.com/so00yeon/go-health-k8s/internal/config"
 	"github.com/so00yeon/go-health-k8s/internal/router"
 )
 
 func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
+
 	cfg := config.Load()
 	mux := router.New(cfg)
 
 	addr := ":" + cfg.Port
-	fmt.Printf("Server listening on %s (env=%s)\n", addr, cfg.AppEnv)
+	slog.Info("server starting", "addr", addr, "env", cfg.AppEnv)
 	if err := http.ListenAndServe(addr, mux); err != nil {
-		log.Fatalf("server failed: %v", err)
+		slog.Error("server failed", "error", err)
+		os.Exit(1)
 	}
 }
